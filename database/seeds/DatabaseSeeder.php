50b861dfc2a81b30b4cb44e7ef72b7813dd15cd4
Ashwin Murali <mail@cruisemaniac.com>
2017-03-21 00:47:31 +0530

    fixing database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,11 +17,30 @@ class DatabaseSeeder extends Seeder
         $faker = Faker::create();
         foreach (range(1,10) as $index) {
             DB::table('users')->insert([
-                'name' => $faker->name,
+                'username' => $faker->username,
+                'firstname' => $faker->firstName,
+                'lastname' => $faker->lastName,
                 'email' => $faker->email,
-                'country' => 'be',
-                'user_id' => '1',
+                'password' => $faker->password,
             ]);
+        }
+        foreach (range(1,5) as $index) {
+            DB::table('mailing_lists')->insert([
+                   'name'  => $faker->company,
+                   'description' => $faker->paragraph,
+                   'public' => 1,
+                   'user_id' => \App\Models\User::find(rand(1,10))->id,
+                ]);
+        }
+        foreach (range(1,100) as $index) {
+            $sub = new \App\Models\Subscription;
+            $sub->email = $faker->email;
+            $sub->name = $faker->name;
+            $sub->country = array_rand(countries());
+            $sub->user_id = \App\Models\User::find(rand(1,10))->id;
+            $sub->mailing_list_id = \App\Models\MailingList::find(rand(1,5))->id;
+
+            $sub->save();
         }
     }
 }
