6f1b36881d4401df8565d7803311ce9a09bf6647
Nathan Geerinck <nathan.geerinck@gmail.com>
2017-06-18 12:44:23 +0200

    CSS + Javascript added

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -41,37 +41,37 @@ $(document).ready( function() {
         e.preventDefault();
         $('#logout-form').submit();
     });
-
-    function deleteEntity(element, subject) {
-        swal({
-            title: "Are you sure you want to delete '" + subject + "'?",
-            text: 'This cannot be undone',
-            type: "warning",
-            showCancelButton: true,
-            cancelButtonText: 'Cancel',
-            confirmButtonColor: "#DD6B55",
-            confirmButtonText: "Delete",
-            closeOnConfirm: false,
-            showLoaderOnConfirm: true
-        }, function () {
-            $(element).closest('form').submit();
-        });
-    }
-
-    function deleteSubscription(element, subject) {
-        swal({
-            title: "Are you sure that you want to unsubscribe?",
-            text: 'This cannot be undone',
-            type: "warning",
-            showCancelButton: true,
-            cancelButtonText: 'Cancel',
-            confirmButtonColor: "#DD6B55",
-            confirmButtonText: "Yes, I'm sure!",
-            closeOnConfirm: false,
-            showLoaderOnConfirm: true
-        }, function () {
-            $(element).closest('form').submit();
-        });
-    }
 });
+
+window.deleteEntity = function (element, subject) {
+    swal({
+        title: "Are you sure you want to delete '" + subject + "'?",
+        text: 'This cannot be undone',
+        type: "warning",
+        showCancelButton: true,
+        cancelButtonText: 'Cancel',
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "Delete",
+        closeOnConfirm: false,
+        showLoaderOnConfirm: true
+    }, function () {
+        $(element).closest('form').submit();
+    });
+};
+
+window.deleteSubscription = function (element, subject) {
+    swal({
+        title: "Are you sure that you want to unsubscribe?",
+        text: 'This cannot be undone',
+        type: "warning",
+        showCancelButton: true,
+        cancelButtonText: 'Cancel',
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "Yes, I'm sure!",
+        closeOnConfirm: false,
+        showLoaderOnConfirm: true
+    }, function () {
+        $(element).closest('form').submit();
+    });
+};
 
