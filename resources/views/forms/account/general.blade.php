485636f24780c127a23b332e3adae652424ea84c
Nathan Geerinck <nathan.geerinck@gmail.com>
2018-01-07 09:24:28 +0100

    For next time

--- resources/views/forms/account/general.blade.php
+++ resources/views/forms/account/general.blade.php
@@ -8,18 +8,14 @@
     {!! Form::email('email', null, ['class' => 'form-control']) !!}
 </div>
 
-<<<<<<< HEAD
-<div class="form-group">
-    {!! Form::label('language', trans('account.general.language') . ' *') !!}
-    {!! Form::select('language', ['en' => trans('language.en'), 'nl' => trans('language.nl')], null, ['class' => 'chosen-select']) !!}
-</div>
+{{--<div class="form-group">--}}
+    {{--{!! Form::label('language', trans('account.general.language') . ' *') !!}--}}
+    {{--{!! Form::select('language', ['en' => trans('language.en'), 'nl' => trans('language.nl')], null, ['class' => 'chosen-select']) !!}--}}
+{{--</div>--}}
 
-<div class="form-group">
-    {!! Form::label('notifications_on', trans('account.general.notifications') . ' *') !!}
-    {!! Form::select('notifications_on', [0 => 'No', 1 => 'Yes'], null, ['class' => 'chosen-select']) !!}
-</div>
+{{--<div class="form-group">--}}
+    {{--{!! Form::label('notifications_on', trans('account.general.notifications') . ' *') !!}--}}
+    {{--{!! Form::select('notifications_on', [0 => 'No', 1 => 'Yes'], null, ['class' => 'chosen-select']) !!}--}}
+{{--</div>--}}
 
 {!! Form::submit(trans('forms.save'), ['class' => 'btn btn-default']) !!}
-=======
-{!! Form::submit(trans('forms.save'), ['class' => 'btn btn-default']) !!}
->>>>>>> b0e1377f1ad00213e56fe3570925e8f049a4895a
